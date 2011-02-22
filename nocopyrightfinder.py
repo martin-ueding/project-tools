b0528c8319fb7127798aa44e34038306b09fef21
Martin Ueding <dev@martin-ueding.de>
2011-02-22 23:11:03 +0100

    made it a plain list of filenames

--- nocopyrightfinder.py
+++ nocopyrightfinder.py
@@ -35,19 +35,6 @@ def checkFiles (arg, dirname, names):
 
 os.path.walk('../../', checkFiles, 0)
 
-if len(bad) > 0:
-	print "The following have no copyright:"
-	for i in sorted(bad):
-		print "-", i
+for i in sorted(bad):
+	print i
 
-	print
-
-if len(good) > 0:
-	print "The following have a copyright:"
-	for i in sorted(good):
-		print "+", i
-
-sum = len(good) + len(bad)
-
-print
-print "That are only "+str(len(good)*100/sum)+"% of your files."

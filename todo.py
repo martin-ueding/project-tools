7d21981d6987c4168989a74c891fc4f04eed0c59
Martin Ueding <dev@martin-ueding.de>
2011-03-06 23:03:53 +0100

    added todo list generator

---
+++ todo.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# Copyright (c) 2010 Martin Ueding <dev@martin-ueding.de>
+
+import os.path
+import re
+import sys
+import optparse
+import readline
+import time
+
+patterns = ['.*\\.java$', '.*\\.php$', '.*\\.cpp$', '.*\\.py$', '^makefile$', '.*\\.html$', '.*\\.js$', '.*\\.sh$', '.*\\.css$']
+cPatterns = []
+
+words = ["(TODO .*)", "(HACK .*)", "(FIXME .*)", "(XXX .*)"]
+
+commentpatterns = []
+for w in words:
+	commentpatterns.append("\\s+//\\s+"+w+"$")	
+	commentpatterns.append("\\s+#\\s+"+w+"$")	
+	commentpatterns.append("\\s+/\\*\\s+"+w+"\\*/$")	
+	commentpatterns.append("\\s+<--\\s+"+w+"-->$")	
+
+cCommentpatterns = []
+
+ignorepatterns = []
+
+for pattern in patterns:
+	cPatterns.append(re.compile(pattern))
+
+for pattern in commentpatterns:
+	cCommentpatterns.append(re.compile(pattern))
+
+def checkFiles (arg, dirname, names):
+	for name in names:
+		path = dirname+'/'+name
+
+		if not any(ignore in path for ignore in ignorepatterns):
+
+			for pattern in cPatterns:
+				if pattern.match(name) != None:
+
+					with open(path, 'r') as script:
+						nameprinted = False
+						for i, line in enumerate(script):
+							
+							for cc in cCommentpatterns:
+								match = cc.match(line)
+								if match != None:
+									if not nameprinted:
+										print
+										print path
+										nameprinted = True
+									print str(i).rjust(4), match.group(1)
+
+
+
+
+
+def main():
+	with open("ignore.txt") as ifile:
+		for line in ifile:
+			ignorepatterns.append(line[:-1])
+
+	print "TODO List generated at",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	
+	os.path.walk('../../', checkFiles, 0)
+
+	
+if __name__ == "__main__":
+	main()

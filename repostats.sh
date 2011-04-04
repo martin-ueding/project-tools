25edaf45782da0133c1c0152cd66137f2f301a68
Martin Ueding <dev@martin-ueding.de>
2011-04-04 17:36:02 +0200

    added c to list, specified repo location

--- repostats.sh
+++ repostats.sh
@@ -2,11 +2,11 @@
 # Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
 
 # get a list of all the files that have my copyright in them
-./nocopyrightfinder.py -g --relax > good
+./nocopyrightfinder.py ../.. -g --relax > good
 
 echo "	files	lines	chars"
 
-for suffix in java php css js html py sh cpp
+for suffix in java php css js html py sh cpp c
 do
 	lines=$(cat `cat good | egrep "\\.$suffix$"` | wc -l)
 	chars=$(cat `cat good | egrep "\\.$suffix$"` | wc -m)

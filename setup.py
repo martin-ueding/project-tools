c4117adc73cd0603af2f4ab7e4279830723d550e
Martin Ueding <dev@martin-ueding.de>
2013-12-10 18:02:32 +0100

    Move scripts, use setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "projecttools",
+    version = "1.6",
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'git-autopush = projecttools.gitautopush:main',
+        ],
+    },
+)

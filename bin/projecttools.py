81aed1518d7ec988be448e6d8de1c699d955f388
Martin Ueding <dev@martin-ueding.de>
2013-12-10 17:37:03 +0100

    Add new projecttools Python 3 module

---
+++ bin/projecttools.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+
+import os.path
+import subprocess
+
+__docformat__ = "restructuredtext en"
+
+def find_git_repos(root='~'):
+    command = [
+        'find', os.path.expanduser(root),
+        '-type', 'd',
+        '-name', '.git',
+        '-print0',
+    ]
+
+    output = subprocess.check_output(command)
+    repos = output.split(b'\0')
+    return repos
+
+if __name__ == '__main__':
+    print(find_git_repos('~/.vim'))

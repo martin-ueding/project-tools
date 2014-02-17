0978e76cc93008226c92a59b79aebc4af38f5351
Martin Ueding <dev@martin-ueding.de>
2014-02-17 13:12:01 +0100

    Add dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2013-2014 Martin Ueding <dev@martin-ueding.de>
 
 from setuptools import setup, find_packages
 
@@ -15,4 +15,7 @@ setup(
             'git-autogc = projecttools.gitautogc:main',
         ],
     },
+    install_requires=[
+        'termcolor'
+    ],
 )
